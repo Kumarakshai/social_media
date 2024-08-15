433f545a0642ba5d755994b48d91726028b30bc5
AkshaiKumar <kumarakshai2425@gmail.com>
2024-08-15 11:35:47 +0530

    Changes

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,6 +1,20 @@
+import { auth } from "@clerk/nextjs/server";
 import Post from "./Post";
+import prisma from "@/lib/clinet";
 
-const Feed = () => {
+const Feed = async ({ username }: { username?: string }) => {
+  const { userId } = auth();
+  let posts;
+
+  if (username) {
+    posts = await prisma.post.findMany({
+      where: {
+        user: {
+          username: username,
+        },
+      },
+    });
+  }
   return (
     <div className="p-4 bg-white shadow-md rounded-2xl flex flex-col gap-12">
       <Post />
--- src/components/rightMenu/RightMenu.tsx
+++ src/components/rightMenu/RightMenu.tsx
@@ -6,7 +6,7 @@ import UserInfoCard from "./UserInfoCard";
 import UserMediaCard from "./UserMediaCard";
 import { Suspense } from "react";
 
-const RightMenu = ({ user }: { user?: User }) => {
+const RightMenu = ({ user }: { user: User }) => {
   return (
     <div className="flex flex-col gap-6">
       {user ? (
